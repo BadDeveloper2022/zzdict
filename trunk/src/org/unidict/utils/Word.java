7bd96c896696a2f697ba794f11d1542fb5da9f1e
zhiheng.zheng <zhiheng.zheng@4d35877c-b41c-11de-997e-c5eb2a8ae348>
2009-10-09 09:24:46 +0000

    add features list & add some functions in the BaseDict class

---
+++ trunk/src/org/unidict/utils/Word.java
@@ -0,0 +1,13 @@
+package org.unidict.utils;
+
+public class Word {
+	
+	/** word */
+	String word;
+	
+	/** word's pronunciation */
+	String pronunciation;
+	
+	/** words' translation */
+	String translation;
+}
