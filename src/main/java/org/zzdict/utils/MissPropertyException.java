857b1d92cf85277413d1a328c6641423ffa3cb48
zhiheng.zheng <zhiheng.zheng@4d35877c-b41c-11de-997e-c5eb2a8ae348>
2009-10-12 10:40:20 +0000

    Parse ifo file using standard Properties class

---
+++ src/main/java/org/zzdict/utils/MissPropertyException.java
@@ -0,0 +1,11 @@
+package org.zzdict.utils;
+
+public class MissPropertyException extends Exception {
+
+	public MissPropertyException(String string) {
+		super(string);
+	}
+
+	private static final long serialVersionUID = -7114193572881066308L;
+
+}
