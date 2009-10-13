0581895f43a6ab32ef0e535593666a47fd557958
zhiheng.zheng <zhiheng.zheng@4d35877c-b41c-11de-997e-c5eb2a8ae348>
2009-10-13 02:32:33 +0000

    format all source files

--- trunk/src/test/java/org/zzdict/utils/WordDataItemTest.java
+++ trunk/src/test/java/org/zzdict/utils/WordDataItemTest.java
@@ -5,9 +5,10 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 public class WordDataItemTest {
-	
+
 	@Test
-	public void testWordDataType(){
-		Assert.assertEquals(WordDataType.valueOf('l'),WordDataType.ANSI_PURE_TEXT);
+	public void testWordDataType() {
+		Assert.assertEquals(WordDataType.valueOf('l'),
+				WordDataType.ANSI_PURE_TEXT);
 	}
 }
