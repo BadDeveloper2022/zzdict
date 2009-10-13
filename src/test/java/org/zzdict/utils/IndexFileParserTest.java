a26fc05564cbdc708e3489d591dbcabb3a872ad1
zhiheng.zheng <zhiheng.zheng@4d35877c-b41c-11de-997e-c5eb2a8ae348>
2009-10-13 02:32:33 +0000

    format all source files

--- src/test/java/org/zzdict/utils/IndexFileParserTest.java
+++ src/test/java/org/zzdict/utils/IndexFileParserTest.java
@@ -14,23 +14,26 @@ public class IndexFileParserTest {
 
 	@Test
 	public void testParseIndexFile() {
-		StarDictIndexFileParser ifp; 
-		Map<String,DictDataInfo> resultMap;
+		StarDictIndexFileParser ifp;
+		Map<String, DictDataInfo> resultMap;
 
 		try {
 			// stardict1.3.idx contains 51214 words file size is 899,574 bytes
-			ifp = new StarDictIndexFileParser("src/test/resources/stardict/stardict1.3-2.4.2/stardict1.3.idx");
+			ifp = new StarDictIndexFileParser(
+					"src/test/resources/stardict/stardict1.3-2.4.2/stardict1.3.idx");
 			resultMap = ifp.parseIndexFile();
-			int i=0;
-			for(Entry<String, DictDataInfo> entry : resultMap.entrySet()){
-				System.out.println("word: "+entry.getKey());
-				System.out.println("offset: "+entry.getValue().wordDataOffset);
-				System.out.println("size: "+entry.getValue().wordDataSize);
+			int i = 0;
+			for (Entry<String, DictDataInfo> entry : resultMap.entrySet()) {
+				System.out.println("word: " + entry.getKey());
+				System.out
+						.println("offset: " + entry.getValue().wordDataOffset);
+				System.out.println("size: " + entry.getValue().wordDataSize);
 				i++;
-				if (i > 1000) break;
+				if (i > 1000)
+					break;
 			}
-			
-			//assert the count of words is correct
+
+			// assert the count of words is correct
 			assertEquals(resultMap.size(), 51214);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -38,7 +41,7 @@ public class IndexFileParserTest {
 			e.printStackTrace();
 		} catch (FileFormatErrorException e) {
 			e.printStackTrace();
-			fail("file format is wrong. Reason is "+e);
+			fail("file format is wrong. Reason is " + e);
 		}
 	}
 
